1bea68f945bdda6c117c71f6c13f1f15dd0fbad9
chiragaiml21 <bharathakur.palwal@gmail.com>
2024-02-25 22:20:38 +0530

    Seceret key hide

--- Frontend/src/Components/Chatbot/TestingBot/TestingBot.jsx
+++ Frontend/src/Components/Chatbot/TestingBot/TestingBot.jsx
@@ -3,7 +3,9 @@ import "../Chatbot.css"
 
 const TestingBot = () => {
   return (
-    <div className={`w-[400px] h-[80vh] rounded-xl bg-white box1`}>TestingBot</div>
+    <div className={`w-[400px] h-[80vh] rounded-xl bg-white box1`}>
+      <h1>Hello</h1>
+    </div>
   )
 }
 
